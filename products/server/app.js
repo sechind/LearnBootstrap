a4558c99f6265d8706df41a2f006cc27fcbebb74
sechind <sechind83@gmail.com>
2021-04-16 16:55:37 +0900

    QUERY 추가

--- products/server/app.js
+++ products/server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const fs = require('fs');
 
 app.use(session({
     secret: 'secret code',
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 1000 * 60 * 60 // 쿠키 유효 시간 1시간
     }
 }));
@@ -16,19 +17,40 @@ const server = app.listen(3000, () => {
     console.log('Server started. port 3000.');
 });
 
+let sql = require('./sql.js');
+
+fs.watchFile(__dirname + '/sql.js', (curr, prev) => {
+    console.log('sql 변경시 재시작 없이 반영되도록 함.');
+    delete require.cache[require.resolve('./sql.js')];
+    sql = require('./sql.js');
+});
+
 const db = require('./db.js');
 
 const dbPool = require('mysql').createPool(db);
 
 app.post('/api/login', async (request, res) => {
-
+    request.session['email'] = 'seungwon.go@gmail.com';
+    res.send('ok');
 });
 
 app.post('/api/logout', async (request, res) => {
-    
+    request.session.destroy();
+    res.send('ok');
 });
 
-const sql = require('./sql.js');
+app.post('/apirole/:alias', async (request, res) => {
+    if(!request.session.email)
+        return res.status(401).send({error:'You need to login.'});
+
+    try {
+        res.send(await req.db(request.params.alias));
+    } catch(err) {
+        res.status(500).send({
+            error: err
+        });
+    }
+});
 
 app.post('/api/:alias', async (request, res) => {
     try {
