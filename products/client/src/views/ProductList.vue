3c18effda4914de9aa8a7ce39cfa830e4d48c274
sechind <sechind83@gmail.com>
2021-04-27 19:09:20 +0900

    제품리스트  vue 완성

--- products/client/src/views/ProductList.vue
+++ products/client/src/views/ProductList.vue
@@ -1,227 +1,57 @@
 <template>
-  <div>
-    <main class="mt-3">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-12">
-            <select class="form-select">
-              <option selected></option>
-              <option value="1">CPU</option>
-              <option value="2">메인보드</option>
-              <option value="3">모바일</option>
-            </select>
-          </div>
+  <main class="mt-3">
+    <div class="container">
+      <div class="row mb-2">
+        <div class="col-12">
+          <select class="form-select">
+            <option selected></option>
+            <option value="1">CPU</option>
+            <option value="2">메인보드</option>
+            <option value="3">모바일</option>
+          </select>
         </div>
-        <div class="row g-3">
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <a href="product_detail.html"><img src="http://img.danawa.com/prod_img/500000/551/308/img/11308551_1.jpg?shrink=360:360&_v=20210106131945" class="card-img-top"></a> 
-              <div class="card-body">
-                <h5 class="card-title">인텔 코어i7-10세대 10700K (코멧레이크S) (정품)</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">컴퓨터</span>
-                  <span class="badge bg-dark">CPU</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">189,900원</small>
+      </div>
+      <div class="row g-3">
+        <div class="col-xl-3 col-lg-4 col-md-6" :key="i" v-for="(product,i) in productList">
+          <div class="card" style="width: 18rem;">
+            <a href="product_detail.html"><img :src="product.path" class="card-img-top"></a> 
+            <div class="card-body">
+              <h5 class="card-title">{{product.product_name}}</h5>
+              <p class="card-text">
+                <span class="badge bg-dark text-white mr-1">{{product.category1}}</span>
+                <span class="badge bg-dark text-white mr-1">{{product.category2}}</span>
+                <span class="badge bg-dark text-white mr-1">{{product.category3}}</span>
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/835/354/img/13354835_1.jpg?shrink=360:360&_v=20210304153901" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">ASUS TUF Gaming Z590-PLUS WIFI 인텍앤컴퍼니</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">컴퓨터</span>
-                  <span class="badge bg-dark">메인보드</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">304,000원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/789/767/img/12767789_1.jpg?shrink=360:360&_v=20201127150936" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">에어 로잉머신 (해외구매)</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">아웃도어</span>
-                  <span class="badge bg-dark">스포츠</span>
-                  <span class="badge bg-dark">홈트레이닝</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">294,140원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/907/986/img/11986907_1.jpg?shrink=360:360&_v=20201102093831" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">삼성전자 갤럭시 워치3 45mm 스테인리스 스틸</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">태블릿/모바일</span>
-                  <span class="badge bg-dark">스마트워치</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">339,840원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/551/308/img/11308551_1.jpg?shrink=360:360&_v=20210106131945" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">인텔 코어i7-10세대 10700K (코멧레이크S) (정품)</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">컴퓨터</span>
-                  <span class="badge bg-dark">CPU</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">189,900원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/835/354/img/13354835_1.jpg?shrink=360:360&_v=20210304153901" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">ASUS TUF Gaming Z590-PLUS WIFI 인텍앤컴퍼니</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">컴퓨터</span>
-                  <span class="badge bg-dark">메인보드</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">304,000원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/789/767/img/12767789_1.jpg?shrink=360:360&_v=20201127150936" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">에어 로잉머신 (해외구매)</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">아웃도어</span>
-                  <span class="badge bg-dark">스포츠</span>
-                  <span class="badge bg-dark">홈트레이닝</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">294,140원</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-lg-4 col-md-6">
-            <div class="card" style="width: 18rem;">
-              <img src="http://img.danawa.com/prod_img/500000/907/986/img/11986907_1.jpg?shrink=360:360&_v=20201102093831" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">삼성전자 갤럭시 워치3 45mm 스테인리스 스틸</h5>
-                <p class="card-text">
-                  <span class="badge bg-dark">전자제품</span>
-                  <span class="badge bg-dark">태블릿/모바일</span>
-                  <span class="badge bg-dark">스마트워치</span>
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">장바구니 담기</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">주문하기</button>
-                  </div>
-                  <small class="text-dark">339,840원</small>
-                </div>
+                <small class="text-dark">{{product.product_price}}원</small>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </main>
-
-    <footer class="mt-5 py-5 bg-dark">
-      <div class="row">
-        <div class="col-12 col-md">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
-          <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Features</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Cool stuff</a></li>
-            <li><a class="link-secondary" href="#">Random feature</a></li>
-            <li><a class="link-secondary" href="#">Team feature</a></li>
-            <li><a class="link-secondary" href="#">Stuff for developers</a></li>
-            <li><a class="link-secondary" href="#">Another one</a></li>
-            <li><a class="link-secondary" href="#">Last time</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Resource name</a></li>
-            <li><a class="link-secondary" href="#">Resource</a></li>
-            <li><a class="link-secondary" href="#">Another resource</a></li>
-            <li><a class="link-secondary" href="#">Final resource</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Business</a></li>
-            <li><a class="link-secondary" href="#">Education</a></li>
-            <li><a class="link-secondary" href="#">Government</a></li>
-            <li><a class="link-secondary" href="#">Gaming</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>About</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Team</a></li>
-            <li><a class="link-secondary" href="#">Locations</a></li>
-            <li><a class="link-secondary" href="#">Privacy</a></li>
-            <li><a class="link-secondary" href="#">Terms</a></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-  </div>
+    </div>
+  </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      productList: []
+    };
+  },
+  created() {
+    this.getProductList();
+  },
+  methods: {
+    async getProductList() {
+      this.productList = await this.$api("/api/productList",{});
+      console.log(this.productList);
+    }
+  }
+}
+</script>
